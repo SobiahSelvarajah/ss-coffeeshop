a2963c0a8bce2464a46f6baf2bf78d1b3896c1dc
Sobiah Selvarajah <sobiahselvarajah@hotmail.co.uk>
2024-06-05 17:08:42 +0100

    src: all pages connected with react router dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/home/Home'
+import OurStory from './pages/ourStory/OurStory'
+import OurProducts from './pages/ourProducts/OurProducts'
+import Contact from './pages/contact/Contact'
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        
+        <Route index element={<Home/>}/>
+        <Route path='ourStory' element={<OurStory/>}/>
+        <Route path='ourProducts' element={<OurProducts/>}/>
+        <Route path='contact' element={<Contact/>}/>
       </Routes>
     </BrowserRouter>
   )
