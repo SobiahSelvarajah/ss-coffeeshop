03872f12427eab999c6287d61984c1851d516241
Sobiah Selvarajah <sobiahselvarajah@hotmail.co.uk>
2024-06-17 13:17:32 +0100

    components: footer contact styled and responsive

--- client/src/components/footerContact/FooterContact.jsx
+++ client/src/components/footerContact/FooterContact.jsx
@@ -3,34 +3,35 @@ import { FaCoffee } from 'react-icons/fa'
 import { FaPhone } from 'react-icons/fa'
 import { MdEmail } from 'react-icons/md'
 import { FaLocationDot } from "react-icons/fa6";
+import './FooterContact.scss'
 
 const FooterContact = () => {
   return (
-    <section className="footer__contact">
-        <h3 className="footer__contactHeading">
+    <section className="footerContact__container">
+        <h3 className="footerContact__heading">
             Contact Info
         </h3>
-        <div className="footer__name">
+        <div className="footerContact__name">
             <FaCoffee/>
-            <h5 className="footer__nameText">
+            <h5 className="footerContact__nameText">
                 My Little Valentine
             </h5>
         </div>
-        <div className="footer__phone">
+        <div className="footerContact__phone">
             <FaPhone/>
-            <h5 className="footer__phoneNumber">
+            <h5 className="footerContact__phoneNumber">
                 (+44) 123 4567 890
             </h5>
         </div>
-        <div className="footer__email">
+        <div className="footerContact__email">
             <MdEmail/>
-            <h5 className="footer__emailText">
+            <h5 className="footerContact__emailText">
                 mlv@example.co.uk
             </h5>
         </div>
-        <div className="footer__address">
+        <div className="footerContact__address">
             <FaLocationDot/>
-            <h5 className="footer__addressText">
+            <h5 className="footerContact__addressText">
                 11 Lovers Lane, City, Country
             </h5>
         </div>
