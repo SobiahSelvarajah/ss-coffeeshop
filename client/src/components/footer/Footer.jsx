f56ec6fa8f8efe952ef44c09e895b95ca67ba026
Sobiah Selvarajah <sobiahselvarajah@hotmail.co.uk>
2024-06-18 20:16:39 +0100

    components: footer opening html added

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import FooterDesc from '../footerDesc/FooterDesc';
 import Socials from '../socials/Socials';
 import FooterContact from '../footerContact/FooterContact';
+import FooterOpening from '../footerOpening/FooterOpening';
 import './Footer.scss'
 
 const Footer = () => {
@@ -15,20 +16,7 @@ const Footer = () => {
             <Socials/>
         </section>
         <FooterContact/>
-        <section className="footer__opening">
-            <h3 className="footer__openingHeading">
-                Opening Times
-            </h3>
-            <h5 className="footer__openingTimes">
-                Mon-Fri: 8AM - 8PM
-            </h5>
-            <h5 className="footer__openingTimes">
-                Sat: 8AM - 4PM
-            </h5>
-            <h5 className="footer__openingTimes">
-                Sun: Closed
-            </h5>
-        </section>
+        <FooterOpening/>
     </footer>
   )
 }
---
+++ client/src/components/footerOpening/FooterOpening.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const FooterOpening = () => {
+  return (
+    <section className="footerOpening__container">
+        <h3 className="footerOpening__heading">
+            Opening Times
+        </h3>
+        <h5 className="footerOpening__times">
+            Mon-Fri: 8AM - 8PM
+        </h5>
+        <h5 className="footerOpening__times">
+            Sat: 8AM - 4PM
+        </h5>
+        <h5 className="footerOpening__times">
+            Sun: Closed
+        </h5>
+    </section>
+  )
+}
+
+export default FooterOpening
